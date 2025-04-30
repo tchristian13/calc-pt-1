0a1460525370d18985d7554e76a02e0438572d06
tchristian13 <tchrstn4@gmail.com>
2025-04-30 03:07:07 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,117 +1,67 @@
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-
-
-
-const buttons = document.querySelectorAll(".button")
-const display = document.getElementByID('display')
-
-buttons.forEach(buttons => {
-        buttons.addEventListener("click",(event) => {
-                buttonPressed(event.target.InnerText)
-                onclick =  buttonPressed(event.target.innerText)
-                })
-        })   
-
-        numberButtons.forEach(button => {
-                button.addEventListener('click', () => {
-                  display.value += button.innerHTML;
+        let firstNumber = ""; 
+        let secondNumber = "";
+        let operator = "";
         
-                })
-        })
+        const buttons = document.querySelectorAll(".button");
+        const display = document.getElementById("display");
         
-function buttonPressed(buttonTitle) {
-        (buttonTitle) => {
-                "-";
-                "+";        
-                "*";
-                "/"; { 
-                        if (firstNumber.length > 0 && operator.length > 0 && secondNumber.length > 0) {
-                                firstNumber = calculate(firstNumber, secondNumber, operator);
-                                operator = buttonTitle;
-                                display()
-                        } else if (firstNumber.length > 0 && operator.length == 0) {
-                                operator = buttonTitle;
-                        } else if (firstNumber.length == 0) {
-                                firstNumber = "0";
-                                operator = buttonTitle;
-                        } else if (secondNumber.length == 0) {
-                                operator = buttonTitle;
-                        } else {
-                                return;  
-                                
-                        }
-
-//  function calculate(num1, num2, operator) {
-//         let result;
-//         switch (operator) {
-//                 case '+':
-//                   result = num1 + num2;
-//                   break;
-//                 case '-':
-//                   result = num1 - num2;
-//                   break;
-//                 case '*':
-//                   result = num1 * num2;
-//                   break;
-//                 case '/':
-//                   if (num2 === 0) {
-//                     return "Cannot divide by zero";
-//                   }
-//                   result = num1 / num2;
-//                   break;
-//                 default:
-//                   return "Invalid operator";
-//               }
-//               return result;
-//             }
-                                                            
-
-                }
-
+        // Helper function to update the display
+        function updateDisplay() {
+          display.value = firstNumber + " " + operator + " " + secondNumber;
         }
-
-        switch (buttonTitle) {
-
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-
-                        if (firstNumber.length > 0)
-                                operator = buttonTitle;
-                        display()
-                        }
-                        break;
         
-                        
-                case "0";
-                case "1";
-                case "2";
-                case "3";
-                case "4";     
-                case "5";
-                case "6";
-                case "7";
-                case "8";      
-                case "9"; {
-                        if (firstNumber.length > 0 && operator.length > 0) {
-                                secondNumber += buttonTitle;
-                                display()
-                        } else {
-                                firstNumber += buttonTitle;
-                                display()
-                        }
-                }
-                break;
-
-                case "C"; {
-                        firstNumber = "";
-                        secondNumber = "";
-                        operator = "";
-                        display()
-                break;                      
+        // Calculate result
+        function calculate(num1, num2, operator) {
+          num1 = parseFloat(num1);
+          num2 = parseFloat(num2);
+        
+          switch (operator) {
+            case "+":
+              return (num1 + num2).toString();
+            case "-":
+              return (num1 - num2).toString();
+            case "*":
+              return (num1 * num2).toString();
+            case "/":
+              return num2 === 0 ? "Error" : (num1 / num2).toString();
+            default:
+              return "Invalid";
+          }
         }
-    
-    
+        
+        // Main button handler
+        function buttonPressed(buttonTitle) {
+          if ("0123456789".includes(buttonTitle)) {
+            if (operator === "") {
+              firstNumber += buttonTitle;
+            } else {
+              secondNumber += buttonTitle;
+            }
+          } else if ("+-*/".includes(buttonTitle)) {
+            if (firstNumber && secondNumber && operator) {
+              firstNumber = calculate(firstNumber, secondNumber, operator);
+              secondNumber = "";
+            }
+            operator = buttonTitle;
+          } else if (buttonTitle === "=") {
+            if (firstNumber && secondNumber && operator) {
+              firstNumber = calculate(firstNumber, secondNumber, operator);
+              secondNumber = "";
+              operator = "";
+            }
+          } else if (buttonTitle === "C") {
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+          }
+        
+          updateDisplay();
+        }
+        
+        
+        buttons.forEach(button => {
+          button.addEventListener("click", event => {
+            const buttonText = event.target.innerText;
+            buttonPressed(buttonText);
+          });
+        });
